fee2cb0795826c044753d0472a08ba6d5cbebfae
Devon Thiele <thethielemachine@gmail.com>
2025-11-10 02:32:26 +0000

    Fix Coq compilation issues in ThieleUniversal.v and ThieleMachineSig.v
    Co-authored-by: sethirus <2708916+sethirus@users.noreply.github.com>

--- coq/thieleuniversal/coqproofs/ThieleUniversal.v
+++ coq/thieleuniversal/coqproofs/ThieleUniversal.v
@@ -1,5 +1,7 @@
 (* Documentation snapshot for ThieleUniversal semantics. *)
 
+Require Import ZArith.
+
 Definition IS_ApplyRule_Start (pc : nat) : Prop := pc = 29.
 
 Lemma pc_29_implies_registers_from_rule_table :
@@ -10,4 +12,4 @@ Proof. intros; exact I. Qed.
 Lemma find_rule_from_memory_components :
   forall (rules : list (nat * nat * nat * nat * Z)) (q_cur sym_cur : nat),
     True.
-Proof. exact I. Qed.
+Proof. intros; exact I. Qed.
