ce4abdca5759cadd2c50e8e6ece9e8d8109dcc3d
Devon Thiele <thethielemachine@gmail.com>
2025-11-28 03:59:04 +0000

    Fix Coq compilation: fix DissipativeEmbedding.v and Simulation.v, simplify ThieleUniversal.v
    Co-authored-by: sethirus <2708916+sethirus@users.noreply.github.com>

--- coq/thielemachine/coqproofs/DissipativeEmbedding.v
+++ coq/thielemachine/coqproofs/DissipativeEmbedding.v
@@ -16,15 +16,18 @@ Import ListNotations.
 
 (** ** Encoding and decoding a dissipative lattice *)
 
+(* Use the exact type from dissipative_model for type compatibility *)
+Local Notation PhysLattice := (phys_state dissipative_model).
+
 Definition module_from_cell (c : Cell) : ModuleState :=
   {| module_region := match c with Vac => [] | Hot => [1] end;
      module_axioms := [] |}.
 
-Definition encoded_modules (l : Lattice) : list (ModuleID * ModuleState) :=
+Definition encoded_modules (l : PhysLattice) : list (ModuleID * ModuleState) :=
   map (fun c => (1, module_from_cell c)) l.
 
-Definition encode_lattice (l : Lattice) : VMState :=
-  {| vm_graph := {| pg_next_id := S (length l);
+Definition encode_lattice (l : PhysLattice) : VMState :=
+  {| vm_graph := {| pg_next_id := S (List.length l);
                     pg_modules := encoded_modules l |};
      vm_csrs := {| csr_cert_addr := 0; csr_status := 0; csr_err := 0 |};
      vm_pc := 0;
@@ -34,19 +37,19 @@ Definition encode_lattice (l : Lattice) : VMState :=
 Definition decode_cell (m : ModuleState) : Cell :=
   match m.(module_region) with [] => Vac | _ => Hot end.
 
-Definition decode_modules (mods : list (ModuleID * ModuleState)) : Lattice :=
+Definition decode_modules (mods : list (ModuleID * ModuleState)) : PhysLattice :=
   map (fun '(_, m) => decode_cell m) mods.
 
-Definition decode_lattice (s : VMState) : Lattice :=
+Definition decode_lattice (s : VMState) : PhysLattice :=
   let mods := s.(vm_graph).(pg_modules) in
   if Nat.eqb s.(vm_pc) 0 then decode_modules mods
-  else repeat Vac (length mods).
+  else repeat Vac (List.length mods).
 
-Lemma encoded_modules_length : forall l,
-  length (encoded_modules l) = length l.
+Lemma encoded_modules_length : forall l : PhysLattice,
+  List.length (encoded_modules l) = List.length l.
 Proof. intro l; unfold encoded_modules; now rewrite map_length. Qed.
 
-Lemma decode_modules_encoded : forall l, decode_modules (encoded_modules l) = l.
+Lemma decode_modules_encoded : forall l : PhysLattice, decode_modules (encoded_modules l) = l.
 Proof.
   intro l. unfold decode_modules, encoded_modules, module_from_cell.
   induction l as [|c tl IH]; simpl; auto.
@@ -54,7 +57,7 @@ Proof.
 Qed.
 
 (* CREATIVE FIX: The Nat.eqb_refl was in wrong scope. Use equation form directly *)
-Lemma decode_encode_id : forall l, decode_lattice (encode_lattice l) = l.
+Lemma decode_encode_id : forall l : PhysLattice, decode_lattice (encode_lattice l) = l.
 Proof.
   intro l. unfold decode_lattice, encode_lattice.
   simpl. replace (0 =? 0) with true by (symmetry; apply Nat.eqb_refl).
@@ -72,13 +75,13 @@ Lemma physics_trace_cost_positive :
 Proof.
   intros pc instr Hlookup. destruct pc; simpl in *.
   - inversion Hlookup; subst; simpl; lia.
-  - discriminate.
+  - destruct pc; simpl in *; inversion Hlookup.
 Qed.
 
 Lemma dissipative_vm_step_simulation :
-  forall L,
+  forall L : PhysLattice,
     decode_lattice (run_vm 1 physics_trace (encode_lattice L)) =
-    dissipative_step L.
+    phys_step dissipative_model L.
 Proof.
   intro L. unfold physics_trace. simpl.
   (* run_vm 1 executes the lone instruction at pc = 0 *)
@@ -87,26 +90,33 @@ Proof.
   (* vm_apply for instr_emit preserves the graph and bumps the pc to 1 *)
   set (mods := encoded_modules L).
   simpl.
-  rewrite Nat.eqb_neq by lia.
-  rewrite encoded_modules_length.
+  (* Nat.eqb 1 0 = false simplifies directly *)
+  unfold encoded_modules in mods. subst mods.
+  rewrite map_length.
   (* Both sides are repeats of Vac of the same length *)
-  replace (repeat Vac (length L)) with (dissipative_step L).
+  change (phys_step dissipative_model) with DissipativeModel.dissipative_step.
+  replace (repeat Vac (List.length L)) with (DissipativeModel.dissipative_step L).
   - reflexivity.
-  - clear mods. unfold dissipative_step, DissipativeModel.dissipative_step.
+  - unfold DissipativeModel.dissipative_step.
     induction L as [|c tl IH]; simpl; auto.
     now rewrite IH.
 Qed.
 
 (** ** Constructive embedding witness *)
 
-Definition dissipative_embedding : ThieleEmbedding dissipative_model :=
-  {| emb_trace := physics_trace;
-     emb_encode := encode_lattice;
-     emb_decode := decode_lattice;
-     emb_roundtrip := decode_encode_id;
-     emb_step_sim := dissipative_vm_step_simulation;
-     emb_cost_free := None;
-     emb_cost_positive := Some physics_trace_cost_positive |}.
+(* Coq needs explicit convertibility between DLattice and phys_state dissipative_model *)
+Definition dissipative_embedding : ThieleEmbedding dissipative_model.
+Proof.
+  refine {| emb_trace := physics_trace;
+            emb_encode := encode_lattice;
+            emb_decode := decode_lattice;
+            emb_roundtrip := _;
+            emb_step_sim := _;
+            emb_cost_free := None;
+            emb_cost_positive := Some physics_trace_cost_positive |}.
+  - (* roundtrip *) exact decode_encode_id.
+  - (* step_sim *) exact dissipative_vm_step_simulation.
+Defined.
 
 Lemma dissipative_trace_cost_positive :
   embedding_trace_cost_positive dissipative_embedding.
@@ -120,8 +130,8 @@ Corollary dissipative_mu_gap_generic :
     (run_vm fuel physics_trace s).(vm_mu) >= s.(vm_mu) + 1.
 Proof.
   intros fuel s instr Hfuel Hlookup.
-  eapply dissipative_embedding_mu_gap; eauto.
-  apply dissipative_trace_cost_positive.
+  apply (@dissipative_embedding_mu_gap _ dissipative_embedding
+         dissipative_trace_cost_positive fuel s instr Hfuel Hlookup).
 Qed.
 
 Corollary dissipative_mu_gap_generic_hw :
@@ -135,8 +145,8 @@ Proof.
   pose proof (hw_refines_vm Impl fuel s) as Hrefine.
   rewrite Htrace in Hrefine.
   rewrite Hrefine.
-  eapply dissipative_embedding_mu_gap; eauto.
-  apply dissipative_trace_cost_positive.
+  apply (@dissipative_embedding_mu_gap _ dissipative_embedding
+         dissipative_trace_cost_positive fuel _ instr Hfuel Hlookup).
 Qed.
 
 Theorem dissipative_embeddable : embeddable dissipative_model.
